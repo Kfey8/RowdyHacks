4db3eba34d8b2f40ef93477ad43a8250828bcf71
okandreajimenez <andrea.jimenez@my.utsa.edu>
2022-03-26 16:37:17 -0500

    List of chronic illnesses
    Want to add "other" later

---
+++ mainJS.js
@@ -0,0 +1,12 @@
+let chronicIllnesses = [
+    {id:1, name: "Heart disease"},
+    {id:2, name: "Stroke"},
+    {id:3, name: "Colorectal cancer"},
+    {id:4, name: "Depression"},
+    {id:5, name: "Eczema"},
+    {id:6, name: "Alzheimers disease"},
+    {id:7, name: "Respiratory Disease"},
+    {id:8, name: "Diabetes"},
+    {id:9, name: "Lupus"},
+    {id:10, name: "Arthritis"},
+]
