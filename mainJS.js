a3dfc1398922b93e055cbe3d81b0b694d7bdf89c
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-26 16:12:33 -0500

    changed login

--- mainJS.js
+++ mainJS.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-    $('#login-trigger').click(function(){
-      $(this).next('#login-content').slideToggle();
-      $(this).toggleClass('active');
+// $(document).ready(function(){
+//     $('#login-trigger').click(function(){
+//       $(this).next('#login-content').slideToggle();
+//       $(this).toggleClass('active');
 
-      if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
-        else $(this).find('span').html('&#x25BC;')
-      })
-  });
-  
+//       if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+//         else $(this).find('span').html('&#x25BC;')
+//       })
+//   });
