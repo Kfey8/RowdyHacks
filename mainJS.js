cac1d69b5cad3c1fca4db2f111f969ed05ac29c9
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-26 17:11:40 -0500

    messing with html

--- mainJS.js
+++ mainJS.js
@@ -10,3 +10,19 @@ let chronicIllnesses = [
     {id:9, name: "Lupus"},
     {id:10, name: "Arthritis"},
 ]
+// function renderIllnesses(chronicIllnesses) {
+//     let html = '<div class="coffee_display col-xs-12 col-lg-6 mt-4">';
+//     html += '<div class="d-flex align-items-baseline">';
+//     html += '<h2 class="name mr-3">' + chronicIllnesses.name + '</h2>';
+//     html += '<p class="roast">' + chronicIllnesses.roast + '</p>';
+//     html += '</div>'
+//     html += '</div>'
+//     return html;
+// }
+// function renderIllness(chronicIllnesses) {
+//     let html = '';
+//     for(let i = 0; i <= chronicIllnesses.length - 1; i++) {
+//         html += renderIllness(chronicIllnesses[i]);
+//     }
+//     return html;
+// }
