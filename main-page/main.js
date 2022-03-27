ed7c405890909594ee958f8003564b0bcad98dfc
kamryn-fey <kamrynfey12@gmail.com>
2022-03-27 03:50:19 -0500

    small changes to main folder

---
+++ main-page/main.js
@@ -0,0 +1,28 @@
+let coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+    {id: 15, name: "Azathoth's Spawn", roast: 'extra-dark'},
+    {id: 16, name: "Hastur's Revenge", roast: 'extra-dark'},
+    {id: 16, name: "Call of Brewhulu", roast: 'extra-dark'},
+    {id: 17, name: "Chthonic Tonic", roast: 'extra-dark'},
+    {id: 18, name: 'Dark Abyss', roast: "extra-dark"},
+];
+
+function display(){
+
+    //Assigning the variable to the user input
+        var userName = document.getElementById("theStory");
+
+}
